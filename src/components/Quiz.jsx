98a8842d1f838fc7b2a754097e5c5afce71e0d8a
Laeti-dev <laetdi@gmail.com>
2023-11-30 07:40:09 +0100

    238 - Adding timer

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 
+import QuestionTimer from "./QuestionTimer.jsx";
+
 import QUESTIONS from '../question.js';
 import quizCompletImg from '../assets/quiz-complete.png'
 
@@ -30,6 +32,9 @@ export default function Quiz() {
   return (
     <div id='quiz'>
       <div id='question'>
+      <QuestionTimer
+        timeout={10000}
+        onTimeout={()=> handleSelectedAnswer(null)} />
           <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
           <ul id='answers'>
             {shuffledAnswer.map((answer) =>
